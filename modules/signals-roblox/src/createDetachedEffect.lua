0d5bd89d25a5e374ee372137686b9994164149ef
Jonathan <jkelaty@roblox.com>
2026-04-24 18:51:47 -0700

    parent to game

--- modules/signals-roblox/src/createDetachedEffect.lua
+++ modules/signals-roblox/src/createDetachedEffect.lua
@@ -26,7 +26,7 @@ local function createDetachedEffect(effect: (scope, dispose) -> ())
 	if isDisposed then
 		dispose()
 	else
-		local bindable = Instance.new("BindableEvent")
+		local bindable = Instance.new("BindableEvent", game)
 		event = bindable
 		bindable.Event:Once(function()
 			bindable:Destroy()
